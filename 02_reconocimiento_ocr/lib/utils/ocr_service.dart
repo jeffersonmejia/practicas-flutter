5755ae59daeecb6ffdfc5b412fff1985004eb4a2
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-03-10 12:56:15 -0500

    feat: update libs on pubsec

--- 02_reconocimiento_ocr/lib/utils/ocr_service.dart
+++ 02_reconocimiento_ocr/lib/utils/ocr_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:ui' as ui;
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf_render/pdf_render.dart';
+import 'package:pdf_render_maintained/pdf_render_maintained.dart';
 import 'package:tesseract_ocr/tesseract_ocr.dart';
 
 class OcrService {
@@ -18,21 +18,17 @@ class OcrService {
         height: page.height.toInt(),
       );
 
-      // Convierte a ui.Image
       final ui.Image uiImage =
           await pageImage!.createImageIfNotAvailable();
 
-      // Convierte ui.Image a PNG bytes
-      final byteData = await uiImage.toByteData(
-          format: ui.ImageByteFormat.png);
+      final byteData =
+          await uiImage.toByteData(format: ui.ImageByteFormat.png);
       final pngBytes = byteData!.buffer.asUint8List();
 
-      // Guarda PNG temporal
       final tempDir = await getTemporaryDirectory();
       final tempFile = File('${tempDir.path}/page_$i.png');
       await tempFile.writeAsBytes(pngBytes);
 
-      // Extrae texto OCR
       final pageText = await TesseractOcr.extractText(tempFile.path);
       allText += pageText + '\n';
     }
