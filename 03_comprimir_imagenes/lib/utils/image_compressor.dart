4b18430b23ccd28afc5b8caf2d0fea58092d59eb
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-03-11 08:42:15 -0500

    fix: flutter issues in dependencies

--- 03_comprimir_imagenes/lib/utils/image_compressor.dart
+++ 03_comprimir_imagenes/lib/utils/image_compressor.dart
@@ -3,11 +3,14 @@ import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 
-Future<File> compressImage(File file, {int quality = 70, int? minWidth, int? minHeight}) async {
-  final dir = await getTemporaryDirectory();
-  final targetPath = path.join(dir.path, 'compressed_${path.basename(file.path)}');
+Future<File> compressImage(File file,
+    {int quality = 70, int? minWidth, int? minHeight}) async {
 
-  final result = await FlutterImageCompress.compressAndGetFile(
+  final dir = await getTemporaryDirectory();
+  final targetPath =
+      path.join(dir.path, 'compressed_${path.basename(file.path)}');
+
+  final XFile? result = await FlutterImageCompress.compressAndGetFile(
     file.absolute.path,
     targetPath,
     quality: quality,
@@ -16,6 +19,9 @@ Future<File> compressImage(File file, {int quality = 70, int? minWidth, int? min
     format: CompressFormat.jpeg,
   );
 
-  if (result == null) throw Exception('No se pudo comprimir la imagen');
-  return result;
+  if (result == null) {
+    throw Exception('No se pudo comprimir la imagen');
+  }
+
+  return File(result.path);
 }
